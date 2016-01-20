67a58d497227b36a5d7e8363e63070fc4f2545b1
Tyler McDonnell <tyler@cs.utexas.edu>
2016-01-19 19:26:06 -0600

    Fixed most outstanding compilation + basic run-time errors.

--- cwr.py
+++ cwr.py
@@ -1,13 +1,15 @@
 import itertools
 import requests
+
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
+from abc import abstractmethod
 from collections import namedtuple, defaultdict
+
 from PyQt4 import QtGui
-from PyQt4 import QtCore
 from PyQt4 import QtWebKit
-from PyQt4.Qt import QUrl
-from abc import ABCMeta, abstractmethod
+from PyQt4.Qt import QUrl, QCheckBox
+
 
 
 class CWR(QtGui.QWidget):
@@ -19,9 +21,9 @@ class CWR(QtGui.QWidget):
         
     def init_UI(self):
 
-        test_rationales = ["The Beatles were an English rock band, formed in Liverpool in 1960. With members John Lennon, Paul McCartney, George Harrison and Ringo Starr, they became widely regarded as the foremost and most influential act of the rock era.", 
-                           "The Beatles were an English rock band, formed in Liverpool in 1960 they came to be perceived as an embodiment of the ideals shared by the counterculture of the 1960s",
-                           "The Beatles were an English rock band The Beatles built their reputation playing clubs in Liverpool and Hamburg over a three-year period from 1960" ]
+        test_rationales = { '0000001' : "The Beatles were an English rock band, formed in Liverpool in 1960. With members John Lennon, Paul McCartney, George Harrison and Ringo Starr, they became widely regarded as the foremost and most influential act of the rock era.", 
+                            '0000002' : "The Beatles were an English rock band, formed in Liverpool in 1960 they came to be perceived as an embodiment of the ideals shared by the counterculture of the 1960s",
+                            '0000003' : "The Beatles were an English rock band The Beatles built their reputation playing clubs in Liverpool and Hamburg over a three-year period from 1960" }
         #test_text = "The Beatles were an English rock band, formed in Liverpool in 1960. With members John Lennon, Paul McCartney, George Harrison and Ringo Starr, they became widely regarded as the foremost and most influential act of the rock era.[1] Rooted in skiffle, beat, and 1950s rock and roll, the Beatles later experimented with several genres, ranging from pop ballads and Indian music to psychedelia and hard rock, often incorporating classical elements in innovative ways. In the early 1960s, their enormous popularity first emerged as Beatlemania, but as the group's music grew in sophistication, led by primary songwriters Lennon and McCartney, they came to be perceived as an embodiment of the ideals shared by the counterculture of the 1960s. The Beatles built their reputation playing clubs in Liverpool and Hamburg over a three-year period from 1960, with Stuart Sutcliffe initially serving as bass player. The core of Lennon, McCartney and Harrison went through a succession of drummers, most notably Pete Best, before asking Starr to join them. Manager Brian Epstein moulded them into a professional act and producer George Martin enhanced their musical potential. They gained popularity in the United Kingdom after their first hit, Love Me Do, in late 1962. They acquired the nickname the Fab Four as Beatlemania grew in Britain over the following year, and by early 1964 they had become international stars, leading the British Invasion of the United States pop market. From 1965 onwards, the Beatles produced what many consider their finest material, including the innovative and widely influential albums Rubber Soul (1965), Revolver (1966), Sgt. Pepper's Lonely Hearts Club Band (1967), The Beatles (commonly known as the White Album, 1968) and Abbey Road (1969). After their break-up in 1970, they each enjoyed successful musical careers of varying lengths. McCartney and Starr, the surviving members, remain musically active. Lennon was shot and killed in December 1980, and Harrison died of lung cancer in November 2001. According to the RIAA, the Beatles are the best-selling music artists in the United States, with 178 million certified units. They have had more number-one albums on the British charts and sold more singles in the UK than any other act. In 2008, the group topped Billboard magazine's list of the all-time most successful Hot 100 artists; as of 2015, they hold the record for most number-one hits on the Hot 100 chart with twenty. They have received ten Grammy Awards, an Academy Award for Best Original Song Score and fifteen Ivor Novello Awards. Collectively included in Time magazine's compilation of the twentieth century's 100 most influential people, they are the best-selling band in history, with estimated sales of over 600 million records worldwide.[2][3] The group was inducted into the Rock and Roll Hall of Fame in 1988, with all four being inducted individually as well from 1994 to 2015."
 
         test_url = 'https://en.wikipedia.org/wiki/The_Beatles'
@@ -70,7 +72,10 @@ class CWR(QtGui.QWidget):
         self._selection_layout = QtGui.QHBoxLayout()
         selection_view.setLayout(self._selection_layout)
         rationale_view.addWidget(selection_view, 1, 0)
-        self.generate_rationale_selection(test_rationales)
+        rationales = []
+        for label, rationale in test_rationales.items():
+            rationales.append(Rationale(label, rationale))
+        self.load_rationales(rationales)
         
         # Statistics View
         self._stat_display = QtGui.QGroupBox("Statistics")
@@ -91,32 +96,26 @@ class CWR(QtGui.QWidget):
         Regenerates data structures and display logic for rationale selection.
         '''
         # Compact holder for all rationale logic.
-        rationale_container = namedtuple('RationaleContainer', ['rationale', 'color', 'display']
+        container = namedtuple('RationaleContainer', ['rationale', 'color', 'display'])
 
         # Friendly display colors used to highlight rationales in source texts.
-        friendly_colors = []
+        colors = [QtGui.QColor(102, 255, 102), # Light Green
+                  QtGui.QColor(255, 102, 102), # Light Red 
+                  QtGui.QColor(102, 201, 255), # Electric Blue
+                  QtGui.QColor(102, 178, 255), # Baby Blue
+                  QtGui.QColor(178, 102, 255), # Light Purple
+                  QtGui.QColor(255, 205, 255), # Light Magenta
+                  QtGui.QColor(255, 102, 178), # Light Pink
+                  QtGui.QColor(192, 192, 192)] # Light Grey
 
         # Re-generate rationale data structures.
-        for r in rationales:
-            self._rationales.append(rationale_container(rational = r, color = None, display = QCheckBox(r.label)))
-    
-    def generate_rationale_selection(self, labels, func):
-        '''
-        Generates and displays rationale checkbox for each provided label.
-
-        Arguments:
-
-        labels -- list of labels, each one of which will map to a checkbox
-        func   -- function that should be called when a checkbox is selected
-        '''
-        colors = [QtGui.QColor("yellow"),
-                  QtGui.QColor("cyan"),
-                  QtGui.QColor("green")]
-        self._rationale_boxes = []
-        for label in labels:
-            new_box = QCheckBox(label)
-            self._rationale_boxes.append(new_box)
-            self._selection_layout.addWidget(new_box)
+        self._rationales = []
+        for i in range(len(rationales)):
+            r = rationales[i]
+            c = colors[min(i, len(colors) - 1)]
+            d = QCheckBox(r.label)
+            self._rationales.append(container(rationale = r, color = c, display = d))
+            self._selection_layout.addWidget(d)
 
     def rationale_selection_changed(self, state):
         '''
@@ -135,8 +134,10 @@ class CWR(QtGui.QWidget):
         # Update display.
         for match in result.matches:
             # Highlight with assigned color for rationale.
+            pass
         for overlap in result.matches:
             # Highlight with overlap color.
+            pass
     
     def update_topic_list(self, topics):
         '''
@@ -223,12 +224,12 @@ class Rationale(object):
                 
         return result(matches = matches, overlap = overlap)
                  
-class HighlightInterface(metaclass=ABCMeta):
+class HighlightInterface(object):
     '''
     '''
     
     @abstractmethod
-    def highlight(self, text, color):
+    def highlight(self, string, color):
         '''
         '''
         pass
@@ -244,7 +245,7 @@ class HighlightWebView(QtWebKit.QWebView, HighlightInterface):
     A special viewer for web URLs which provides highlight functionality.
     '''
 
-    def highlight(self, text, color):
+    def highlight(self, string, color):
         '''
         '''
         pass
@@ -315,8 +316,10 @@ if __name__ == "__main__":
     window.update_topic_list(["978", "1067", "1065"])
     window.update_document_list(["https://en.wikipedia.org/wiki/Taylor_Swift", "https://en.wikipedia.org/wiki/The_Beatles"])
     
+    '''
     web = QtWebKit.QWebView()
     web.load(QUrl('https://en.wikipedia.org/wiki/The_Beatles'))
     web.show()
+    '''
     
     sys.exit(a.exec_())
